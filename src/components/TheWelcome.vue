26679ab766c90ab41837e709d8db311248bda4a8
unknown <hamzaelbadaoui800@gmail.com>
2023-08-20 23:12:47 +0100

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,63 @@
+
+
+<template>
+
+<div>
+    <h1>hello</h1>
+    <p>first project</p>
+    <select v-model="selectedHotel">
+      <option value="">Select a hotel</option>
+      <option v-for="hotel in hotels" :key="hotel.id" :value="hotel.id">{{ hotel.email }}</option>
+    </select>
+    <div v-if="selectedHotel">
+      <h2>Selected Hotel: {{ getSelectedHotel(selectedHotel).email }}</h2>
+    </div>
+  </div>
+      {{ hotels}}
+sssssssssss
+      <CardsHotel/>
+  
+</template>
+
+
+<script >
+
+  import axios from 'axios';
+  import CardsHotel from './CardsHotel.vue';
+  import { ref } from 'vue'
+
+      
+      export default {
+         components(){
+          CardsHotel 
+         },
+        setup()
+        {
+          const hotels = ref('')
+                  return {
+                    hotels
+                  }
+        },
+        mounted () {
+        axios
+          .get('http://127.0.0.1:8000/api/hotel/all')
+          .then(response => {
+              this.hotels = response.data 
+          })
+         
+          .catch(error => {
+              console.error('Error fetching data:', error);
+            });
+          
+      }
+
+    }
+    
+    
+</script>
+
+
+
+<style>
+
+</style>
